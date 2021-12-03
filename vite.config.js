47c20e394a4fc6d345580e841143ec9e6e2a5be2
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-12-03 17:03:03 +0900

    fix・settings: 細かな対応漏れ(#6)
    index.htmlにdescription追加
    StyleLint、PrettierのglobをWindowsに対応
    Viteのbaseを修正

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/ch-recipe-page/',
+  base: process.env.NODE_ENV === 'production' ? '/ch-recipe-page/' : './',
   server: {
     open: true,
   },
